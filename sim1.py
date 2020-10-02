75c1eecd68dc08664960df86af9209cf091be6f1
Anne Ouyang <anne.ouyang@gmail.com>
2020-10-02 16:55:24 -0400

    fix mine & check convergence issue

--- sim1.py
+++ sim1.py
@@ -2,9 +2,9 @@ import random
 import matplotlib.pyplot as plt
 import time
 
-WINDOW_LEN = 20
-MAX_ROUNDS = 1000
-p = .002
+WINDOW_LEN = 10
+MAX_ROUNDS = 10000
+p = .0002
 n = 1000
 
 num_blocks_mined_total = 1
@@ -46,19 +46,10 @@ def tie_break_by_random(longest_chains, node_id):
 	return random.choice(longest_chains)
 
 
-def mine_block(node_id, longest_chains, tie_breaking_function, r):
-	global num_blocks_mined_total
-	global new_blocks_produced
-	_ = random.uniform(0, 1)
-	if _ >= p:
-		return
+def update_longest_chain(node_id, longest_chains, tie_breaking_function, r):
 	block_to_mine_on = tie_breaking_function(longest_chains, node_id)
-	new_block_id = num_blocks_mined_total
-	num_blocks_mined_total += 1
-	new_blocks_produced += 1
-	blocks[new_block_id] = (blocks[block_to_mine_on][0] + 1, block_to_mine_on, node_id, r)
-	nodes[node_id]['blocks'][blocks[new_block_id][0]] = new_block_id
-	nodes[node_id]['height'] = blocks[new_block_id][0]
+	nodes[node_id]['height'] = blocks[block_to_mine_on][0]
+	nodes[node_id]['blocks'][blocks[block_to_mine_on][0]] = block_to_mine_on
 	cur_height = nodes[node_id]['height']
 	# remove blocks out the window range (to save memory)
 	hkeys = list(nodes[node_id]['blocks'].keys())
@@ -70,6 +61,22 @@ def mine_block(node_id, longest_chains, tie_breaking_function, r):
 	while cur_height > min_lim:
 		nodes[node_id]['blocks'][cur_height - 1] = blocks[nodes[node_id]['blocks'][cur_height]][1]
 		cur_height -= 1
+
+
+def mine_block(node_id, longest_chains, tie_breaking_function, r):
+	global num_blocks_mined_total
+	global new_blocks_produced
+	_ = random.uniform(0, 1)
+	if _ > p:
+		return
+	block_to_mine_on = nodes[node_id]['blocks'][nodes[node_id]['height']]
+	new_block_id = num_blocks_mined_total
+	num_blocks_mined_total += 1
+	new_blocks_produced += 1
+	blocks[new_block_id] = (blocks[block_to_mine_on][0] + 1, block_to_mine_on, node_id, r)
+	nodes[node_id]['blocks'][blocks[new_block_id][0]] = new_block_id
+	nodes[node_id]['height'] = blocks[new_block_id][0]
+	
 
 def check_for_convergence():
 	global max_convergence_height
@@ -94,12 +101,14 @@ def main():
 	prev_round_only_one_node_mined = 1
 	random.seed(time.time())
 	for r in range(1, MAX_ROUNDS + 1):
-		# if (r % 100 == 0):
-			# print("round ", r)
+		if (r % 1000 == 0):
+			print("round ", r)
 		longest_chains = get_longest_chains()
 		for i in range(n):
-			mine_block(i, longest_chains, tie_break_by_random, r)
+			update_longest_chain(i, longest_chains, tie_break_by_random, r)
 		check_for_convergence()
+		for i in range(n):
+			mine_block(i, longest_chains, tie_break_by_random, r)
 		consensus_height_by_round[r] = max_convergence_height
 		if new_blocks_produced == 1:
 			rounds_interval_only_one_node_mine.append(r - prev_round_only_one_node_mined)
